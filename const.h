57c06b4585b086df1fe8b14743d9d28b28b058ab
James Feng Cao <jamesfengcao@yahoo.com>
2016-04-01 18:26:02 +0800

    change useragent back

--- const.h
+++ const.h
@@ -1,3 +1,3 @@
 /* #define USAGE "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36" */
-#define USAGE "netdisk;4.4.0.6;PC;PC-Windows;6.2.9200;WindowsBaiduYunGuanJia"
+#define USAGE "Mozilla/5.0 (X11; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0 Light/36.0"
 
